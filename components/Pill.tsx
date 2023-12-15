9499f2476ae04f88d6b2a09005db246be60d1ec7
tim connors <tim@101.xyz>
2023-12-15 14:03:52 -0800

    changed pill color and line height

--- components/Pill.tsx
+++ components/Pill.tsx
@@ -1,6 +1,6 @@
 export default function Pill({ text }: { text: string }) {
   return (
-    <span className="inline-block bg-orange-600 rounded-full px-1.5 py-0.5 text-xs font-normal text-orange-100 mx-1 mb-1">
+    <span className="inline-block bg-purple-500 rounded-full px-1.5 py-0.5 text-xs font-normal text-purple-100 mx-1 mb-1 leading-none align-middle">
       {text}
     </span>
   );
