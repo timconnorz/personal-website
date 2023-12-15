f1e8ad29ff3952afc748a63d11e89613862e6255
tim connors <tim@101.xyz>
2023-12-15 15:36:54 -0800

    fixed back button

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 
 ## To set an open graph image:
 - Add a file 'opengraph-image.png' to the same location where page.mdx can be found
+
+## TO set a page title and description
+- Use frontmatter variables 'title' and 'description' at the top of the page.mdx file
--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { usePathname } from 'next/navigation'
 
 export default function PostLayout({
   children,
@@ -9,9 +10,21 @@ export default function PostLayout({
   children: React.ReactNode;
 }) {
   const router = useRouter();
+  const pathname = usePathname()
+
+  console.log(pathname)
+
+  const goBack = () => {
+    const path = pathname.split('/');
+    path.pop();
+    let newPath = path.join('/');
+    if (newPath === "/posts") newPath = "/";
+    router.push(newPath);
+  }
+
   return (
     <div className="mt-8">
-      <button onClick={() => router.back()}>
+      <button onClick={goBack}>
         <Image
           src="/left-caret.svg"
           alt="left caret"
