4a9c6a52882b8e7617b66ac0b0aebef6c55e729b
tim connors <tim@101.xyz>
2023-12-19 02:16:50 -0800

    visual tweak to filters

--- components/FilterPill.tsx
+++ components/FilterPill.tsx
@@ -1,11 +1,12 @@
+import { Badge } from "@/components/ui/badge"
 
 export default function FilterPill({ active, onClick, children }: { active: boolean, onClick: () => void, children: React.ReactNode }) {
   return (
     <button
-      className={`filter-pill ${active ? "active bg-gray-950" : ""} bg-gray-500 rounded-full px-2.5 py-1 text-xs font-normal text-gray-100 leading-none`}
       onClick={onClick}
     >
-      {children}
+      <Badge onClick={onClick} variant={active ? "default" : "outline"}>{children}</Badge>
+
     </button>
   );
 }
